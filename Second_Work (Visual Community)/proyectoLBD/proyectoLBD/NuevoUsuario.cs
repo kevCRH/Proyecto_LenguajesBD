3a6c5ed5dcc1a15a1e3ec6ecc05e55175215ac1e
Jose P Retana <joseretanap@ufide.ac.cr>
2022-11-13 10:25:24 -0600

    Interfaz de registro usuario

---
+++ Second_Work (Visual Community)/proyectoLBD/proyectoLBD/NuevoUsuario.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectoLBD
+{
+    public partial class NuevoUsuario : Form
+    {
+        public NuevoUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void panel_fondoRojo_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void txt_navBar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel_fondoRojo_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void NuevoUsuario_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'dataSet1.TBROL' Puede moverla o quitarla según sea necesario.
+            this.tBROLTableAdapter.Fill(this.dataSet1.TBROL);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
